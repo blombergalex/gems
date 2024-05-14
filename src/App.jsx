143a582cf4d5711f8491d0fc986a5e55e20000c1
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-05-14 15:10:39 +0200

    create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import Layout from './pages/Layout'
+import { Routes, Route } from 'react-router-dom';
+import DetailedPage from './pages/DetailedPage';
 import './App.css'
 
 function App() {
@@ -7,7 +9,13 @@ function App() {
   
   return (
     <>
-      <Layout selectedProduct={selectedProduct} setSelectedProduct={setSelectedProduct}/>
+      <Routes>
+        <Route path='/' element={ <Layout selectedProduct={selectedProduct} setSelectedProduct={setSelectedProduct}/> }>
+          <Route path='crystals' element={<DetailedPage/>} />
+          <Route path='essentialoils' element={<DetailedPage/>} />
+          <Route path='incenses' element={<DetailedPage/>}/>
+        </Route>
+      </Routes>
     </>
   )
 }
---
+++ src/pages/DetailedPage/index.jsx
@@ -0,0 +1,18 @@
+
+const DetailedPage = ({product}) => {
+    return(
+        <div>
+            <p>Detailed page</p>
+            {/* <img src={product.image} alt={`Image of ${product.name}`}/>
+            <div>
+                <h3>{product.name}</h3>
+                <p>{product.shortDescription}</p>
+                <p>{product.price}</p>
+                <p>{product.origin}</p>
+                <p>{product.category}</p>
+            </div> */}
+        </div> 
+    )
+}
+
+export default DetailedPage;
