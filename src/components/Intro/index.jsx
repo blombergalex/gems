7a2bc82ebee7dcd6c53b2877d4cefb25374b139e
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-05-21 14:40:06 +0200

    add timer for fadeOut

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -1,15 +1,30 @@
-import styles from './Intro.module.css'
+import { useEffect, useState } from 'react';
 import { Lotus } from '../Lotus';
+import styles from './Intro.module.css'
 
 const Intro = () => {
+
+    const [showIntro, setShowIntro] = useState(true);
+    const [fadeOut, setFadeOut] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setFadeOut(true);
+            setTimeout(()=> {
+                setShowIntro(false);
+            }, 1000);
+        }, 3000);
+        
+        return () => clearTimeout(timer);
+    }, []);
+
     return(
-        <div className={styles.intro}>
+        <div className={`${styles.intro} ${ fadeOut ? styles.fadeOut : ''}`}>
             <div className={styles.lotusContainer}>
                 <Lotus className={styles.lotus}/>
             </div>
             <p className={styles.introText}>Zen Gem Haven</p>
         </div>
-    )
+        )
 }
-
 export default Intro;
