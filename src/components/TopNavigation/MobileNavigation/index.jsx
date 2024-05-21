1e3cfc928c39e301187851fcf29c176b3ba558f1
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-05-21 20:16:33 +0200

    create fade out for close menu

--- src/components/TopNavigation/MobileNavigation/index.jsx
+++ src/components/TopNavigation/MobileNavigation/index.jsx
@@ -1,11 +1,11 @@
 import { Menu, X } from 'lucide-react'
 import { NavLink } from 'react-router-dom'
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import styles from './MobileNavigation.module.css'
 
 const MobileNavigation = ({handleCategoryChange}) => {
-
     const [showNavItems, setShowNavItems] = useState(false);
+    const [isClosing, setIsClosing] = useState(false);
     const navItemsContainer = useRef(null);
     
     const handleMenuClick = () => {
@@ -13,9 +13,12 @@ const MobileNavigation = ({handleCategoryChange}) => {
     }
 
     const closeMenu = () => {
-        setShowNavItems(false)
-        
-    }
+        setIsClosing(true);
+        setTimeout(() => {
+            setShowNavItems(false)
+            setIsClosing(false);
+        }, 1000);
+    };
 
     document.addEventListener('click', (event) => {
         if (showNavItems && !navItemsContainer?.current?.contains(event.target)) {
@@ -27,7 +30,7 @@ const MobileNavigation = ({handleCategoryChange}) => {
         <>
         <nav className={styles.navbar}>
             {showNavItems &&
-            <div className={styles.navItemsContainer}>
+            <div className={`${styles.navItemsContainer} ${isClosing ? styles.fadeOut : ''}`}>
                 <NavLink className={({ isActive}) => isActive ? styles.active : ''} to='/' onClick={() => handleCategoryChange('home')} >Home</NavLink>
                 <NavLink className={({ isActive}) => isActive ? styles.active : ''} to='crystals' onClick={() => handleCategoryChange('crystals')} >Crystals</NavLink>
                 <NavLink className={({ isActive}) => isActive ? styles.active : ''} to='essentialoils' onClick={() => handleCategoryChange('essentialoils')} >Essential Oils</NavLink>
