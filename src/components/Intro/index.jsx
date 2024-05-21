b3b53fbb3cb9806aa534fe328e3764c592741d13
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-05-21 15:06:02 +0200

    control element visibility with state

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -3,28 +3,29 @@ import { Lotus } from '../Lotus';
 import styles from './Intro.module.css'
 
 const Intro = () => {
-
-    const [showIntro, setShowIntro] = useState(true);
     const [fadeOut, setFadeOut] = useState(false);
+    const [visible, setVisible] = useState(true);
 
     useEffect(() => {
         const timer = setTimeout(() => {
             setFadeOut(true);
             setTimeout(()=> {
-                setShowIntro(false);
+                setVisible(false);
             }, 1000);
         }, 3000);
-        
+
         return () => clearTimeout(timer);
     }, []);
-
+    
     return(
+        visible ? (
         <div className={`${styles.intro} ${ fadeOut ? styles.fadeOut : ''}`}>
             <div className={styles.lotusContainer}>
                 <Lotus className={styles.lotus}/>
             </div>
             <p className={styles.introText}>Zen Gem Haven</p>
         </div>
-        )
-}
+        ): null
+)};
+
 export default Intro;
