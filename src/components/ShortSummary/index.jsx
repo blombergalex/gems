4a5ba6e38927868a657cc6bb300b35c40b379f83
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-05-22 18:21:30 +0200

    small fixes

--- src/components/ShortSummary/index.jsx
+++ src/components/ShortSummary/index.jsx
@@ -7,7 +7,7 @@ const ShortSummary = ({product, toggleFullSummary}) => {
             <img src={product.image} alt={`Image of ${product.name}`} className={styles.smallImage}/>
             <div className={styles.infoBox}>
                 <h3>{product.name}</h3>
-                <p>{product.shortDescription}</p>
+                <p className={styles.shortDescription}>{product.shortDescription}</p>
                 <p>Origin: {product.origin}</p>
                 <p>{product.price} kr</p>
                 <p>Find more <span>{product.category}</span></p>
