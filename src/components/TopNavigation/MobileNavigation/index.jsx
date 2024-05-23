31bfa730685a89d91e83b172b195a81c09e77a2f
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-05-23 12:58:17 +0200

    remove eventlistener and add closemenu to each component in navigation to resolve menu closing on menu click bug

--- src/components/TopNavigation/MobileNavigation/index.jsx
+++ src/components/TopNavigation/MobileNavigation/index.jsx
@@ -9,32 +9,28 @@ const MobileNavigation = ({handleCategoryChange}) => {
     const navItemsContainer = useRef(null);
     
     const handleMenuClick = () => {
-        setShowNavItems(!showNavItems)
+        setShowNavItems(true)     
+        console.log('menu icon clicked')
     }
 
     const closeMenu = () => {
         setIsClosing(true);
+
         setTimeout(() => {
             setShowNavItems(false)
             setIsClosing(false);
         }, 2000);
     };
 
-    document.addEventListener('click', (event) => {
-        if (showNavItems && !navItemsContainer?.current?.contains(event.target)) {
-            closeMenu();
-        }
-    });
-
     return(
         <nav className={styles.navbar}>
             {showNavItems &&
             <div className={`${styles.navItemsContainer} ${isClosing ? styles.fadeOut : ''}`}>
-                <NavLink className={({ isActive}) => isActive ? styles.active : ''} to='/' onClick={() => handleCategoryChange('home')} >Home</NavLink>
-                <NavLink className={({ isActive}) => isActive ? styles.active : ''} to='crystals' onClick={() => handleCategoryChange('crystals')} >Crystals</NavLink>
-                <NavLink className={({ isActive}) => isActive ? styles.active : ''} to='essentialoils' onClick={() => handleCategoryChange('essentialoils')} >Essential Oils</NavLink>
-                <NavLink className={({ isActive}) => isActive ? styles.active : ''} to='incenses' onClick={() => handleCategoryChange('incenses')}>Incenses</NavLink>
-                <X className={styles.closeMenuIcon}/>
+                <NavLink className={({ isActive}) => isActive ? styles.active : ''} to='/' onClick={() => {handleCategoryChange('home'); closeMenu()}} >Home</NavLink>
+                <NavLink className={({ isActive}) => isActive ? styles.active : ''} to='crystals' onClick={() => {handleCategoryChange('crystals'); closeMenu()}}>Crystals</NavLink>
+                <NavLink className={({ isActive}) => isActive ? styles.active : ''} to='essentialoils' onClick={() => {handleCategoryChange('essentialoils'), closeMenu()}}>Essential Oils</NavLink>
+                <NavLink className={({ isActive}) => isActive ? styles.active : ''} to='incenses' onClick={() => {handleCategoryChange('incenses'), closeMenu()}}>Incenses</NavLink>
+                <X onClick={closeMenu} className={styles.closeMenuIcon}/>
             </div>
             }
             <Menu onClick={handleMenuClick} ref={navItemsContainer} className={styles.menuIcon}/>
