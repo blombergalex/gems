dd7f01bc9f0d29fa3ef8ca6b80619068f62f4b89
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-11-15 16:04:09 +0100

    fix header classname to work on vercel

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,7 +2,7 @@ import styles from './Header.module.css'
 
 export const Header = ({category}) => {
   return(
-    <header>
+    <header className='header'>
       <div  className='logo'><img src="/images/lotus.png" alt='zgh logo' height={60}/></div>
       <h1>{category}</h1>
     </header>
