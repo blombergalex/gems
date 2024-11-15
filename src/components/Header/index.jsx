6a024711e51dc720c85b891a0c94ec75e4b59054
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-11-15 16:12:52 +0100

    fix header style reference

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,8 +2,8 @@ import styles from './Header.module.css'
 
 export const Header = ({category}) => {
   return(
-    <header className='header'>
-      <div  className='logo'><img src="/images/lotus.png" alt='zgh logo' height={60}/></div>
+    <header className={styles.header}>
+      <div  className={styles.logo}><img src="/images/lotus.png" alt='zgh logo' height={60}/></div>
       <h1>{category}</h1>
     </header>
   )
